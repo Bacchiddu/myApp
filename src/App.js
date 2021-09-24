d31a477ff414a54016d3e6c84edac0e7546fe33e
Levi Bacchiddu <bacchiddulevi04@gmail.com>
2021-09-24 16:32:29 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NavBar from '../src/componentes/NavBar'; 
+import './styles.css';
+
+function App() {
+  return <>
+    <NavBar/>
+  </> 
+} 
+
+// volver a activar si saco el export default
+export default App; 
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,25 @@
+import './NavBar.css';
+/* import './styless.css'; */
+export default function NavBar() {
+    return (
+      <div className="NavBarCompleto">     
+        <div className="NavBar">
+            <ul>
+                <li>
+                    <a href="#">Homesssnps</a>
+                </li>
+                <li>
+                    <a href="#">Redes</a>
+                </li>
+                <li>
+                    <a href="#">Nosotros</a>
+                </li>
+                <li>
+                    <a href="#">Carrito</a>
+                </li>
+                
+            </ul>
+        </div>
+      </div>
+    );
+  }
